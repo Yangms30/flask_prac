62befa771d3c12148c0453ebdcca5c616bf2a904
양민석 <yms090412@gmail.com>
2024-10-17 11:24:09 +0900

    개발 환경 및 운영 환경 환경변수 설정

---
+++ config/default.py
@@ -0,0 +1,2 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # 이 default.py가 MYproject/config에 있으므로 BASE_DIR = os.path.dirname(MYPROJECT/config) = > BASE_DIR = myproject가 됨
--- config/production.py
+++ config/production.py
@@ -1,7 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+SECRET_KEY = b'\xdeO\x19\x8ao\xa7\xc3\xf0\xfeA\xf7\x84\x17eo\xe9'
